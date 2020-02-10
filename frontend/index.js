dd30e00f19dbe4d789ba16f7ee835ae67f33f906
Heather <heather.miller114@gmail.com>
2020-02-09 22:22:36 -0500

    moves script from index.html to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,19 @@
-//retrieves the node in the DOM
-const canvas = document.getElementById('tutorial');
+    //ok but I need to get rid of this script from HTML
+    function draw() {
+        const canvas = document.getElementById('tutorial');
 
-//this will let me access the drawing context
-const ctx = canvas.getContext('2d');
+        //this will let me access the drawing context
+        const ctx = canvas.getContext('2d');
 
-ctx.font = "30px Arial"
-ctx.fillText("Hello World", 10, 50);
+        ctx.beginPath();
+        ctx.moveTo(75, 25);
+        ctx.lineTo(25, 150);
+        ctx.lineTo(125, 150);
+
+        const img = new Image();
+        img.src = 'https://images.app.goo.gl/ZdCkkymqDNpu3Tr59'
+        img.onload = function() {
+            const pattern = ctx.createPattern(img, 'repeat');
+            ctx.fillStyle = pattern
+        }
+    }
