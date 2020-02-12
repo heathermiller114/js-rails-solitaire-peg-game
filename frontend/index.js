0e807a618c521e8ac4f320d8b712d9729369c841
Heather <heather.miller114@gmail.com>
2020-02-12 12:43:12 -0500

    adds 4 rows of spots on board

--- frontend/index.js
+++ frontend/index.js
@@ -5,11 +5,63 @@
         //this will let me access the drawing context
         const ctx = canvas.getContext('2d');
 
+        // triangle shape 
+
         ctx.fillStyle = 'Purple'
 
         ctx.beginPath();
-        ctx.moveTo(75, 25);
-        ctx.lineTo(25, 150);
-        ctx.lineTo(125, 150);
+        ctx.moveTo(75, 0);
+        ctx.lineTo(0, 150);
+        ctx.lineTo(150, 150);
+        ctx.fill();
+
+        // row 1 circle shape
+
+        ctx.fillStyle = 'White'
+
+        ctx.beginPath()
+        ctx.arc(75, 35, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        // row 2 circle shapes
+
+        ctx.beginPath()
+        ctx.arc(60, 70, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(90, 70, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        //row 3 circle shapes
+
+        ctx.beginPath()
+        ctx.arc(50, 105, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(75, 105, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(100, 105, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+        
+        // row 4 circle shapes
+
+        ctx.beginPath()
+        ctx.arc(30, 135, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(60, 135, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(90, 135, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+
+        ctx.beginPath()
+        ctx.arc(120, 135, 10, 0, Math.PI * 2, true);
         ctx.fill();
     }
