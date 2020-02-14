53e08818dfeacbdb89c27bcc34bfd02340f20e3e
Heather <heather.miller114@gmail.com>
2020-02-14 16:13:41 -0500

    new routes -- trying to get devise views to render in the browser

--- backend/js-rails-app/config/routes.rb
+++ backend/js-rails-app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "users#sign_in
+  
+  devise_scope  :user do
+    get "sign_in", to: "devise/sessions#new"
+    get "sign_up", to: "devise/registrations#new"
+  end
 end
