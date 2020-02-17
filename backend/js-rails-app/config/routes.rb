db4c93356c5739d49c1379ce8fa5f9e75054bcb3
Heather <heather.miller114@gmail.com>
2020-02-17 17:24:18 -0500

    adds devise_token_auth

--- backend/js-rails-app/config/routes.rb
+++ backend/js-rails-app/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth'
+  #devise_for :users
   
-  devise_scope  :user do
-    scope :auth, defaults: { format: :json } do    
-      get "sign_in", to: "devise/sessions#new"
-      get "sign_up", to: "devise/registrations#new"
-    end
-  end
+  #devise_scope  :user do
+  #  scope :auth, defaults: { format: :json } do    
+  #    get "sign_in", to: "devise/sessions#new"
+  #    get "sign_up", to: "devise/registrations#new"
+ #   end
+  #end
 end
