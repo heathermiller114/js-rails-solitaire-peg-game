0bfbbd4f45b77ec5cc8c5117df202ae5c9ca16bc
Heather <heather.miller114@gmail.com>
2020-02-17 17:02:35 -0500

    removes devise files

--- backend/js-rails-app/config/routes.rb
+++ backend/js-rails-app/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   devise_scope  :user do
-    get "sign_in", to: "devise/sessions#new"
-    get "sign_up", to: "devise/registrations#new"
+    scope :auth, defaults: { format: :json } do    
+      get "sign_in", to: "devise/sessions#new"
+      get "sign_up", to: "devise/registrations#new"
+    end
   end
 end
